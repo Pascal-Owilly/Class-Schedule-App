19756ff81fccbbdb131c583cb252736b1f7444bd
danknjoroge <dank8505@gmail.com>
2022-04-27 12:34:18 +0300

    creates amnnouncement view class

--- scheduleapp/views.py
+++ scheduleapp/views.py
@@ -1,4 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AnnouncementSeializers
+from .models import Announcements
 
+# Create your views here.
+class AnnouncementsList(viewsets.ModelViewSet):
+    serializers_class = AnnouncementSeializers
+    queryset = Announcements.objects.all()
+    
 
 
 
