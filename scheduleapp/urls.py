7682c464cf1963ad062041adaa43d21e21fcf917
antonymburia <antonymburia10@gmail.com>
2022-05-03 09:38:51 +0300

    update urls

--- scheduleapp/urls.py
+++ scheduleapp/urls.py
@@ -8,7 +8,7 @@ router.register(r'announcements', AnnouncementsList)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('api/comments/', views.CommentList.as_view()),
+    path('comments/', views.CommentList.as_view()),
     re_path(r'api/comments/(?P<pk>[0-9]+)/$',views.SingleComment.as_view()),
     path('sessions/', views.Sessions.as_view()),
     re_path(r'api/sessions/(?P<pk>[0-9]+)/$', views.SingleSession.as_view()),
