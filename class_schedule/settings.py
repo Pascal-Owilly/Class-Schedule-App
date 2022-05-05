83fa4d10d8b8004d3cb61da2796d2aa3ebb70a9e
antonymburia <antonymburia10@gmail.com>
2022-05-05 10:37:45 +0300

    update views

--- class_schedule/settings.py
+++ class_schedule/settings.py
@@ -164,11 +164,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 django_heroku.settings(locals())
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#     ]
+# }
 
 
 # AUTH_USER_MODEL="scheduleapp.Profile"
