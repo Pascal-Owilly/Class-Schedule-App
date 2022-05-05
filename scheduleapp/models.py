907bc35027170dc635021a265dae5af722c5cfcf
antonymburia <antonymburia10@gmail.com>
2022-05-05 10:31:22 +0300

    update models

--- scheduleapp/models.py
+++ scheduleapp/models.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token
 
 
 from cloudinary.models import CloudinaryField
@@ -27,10 +27,10 @@ class Profile(models.Model):
   def delete_profile(self): 
     self.delete()
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def create_auth_token(sender, instance=None, created=False, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
 
 class Course(models.Model):
  course_name = models.CharField(max_length=500)
