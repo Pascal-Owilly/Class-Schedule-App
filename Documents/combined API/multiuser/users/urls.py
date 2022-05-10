8b552b457d9a2cbdc6999a017f77c390c2791447
danknjoroge <dank8505@gmail.com>
2022-05-10 14:58:36 +0300

    combined models

--- Documents/combined API/multiuser/users/urls.py
+++ Documents/combined API/multiuser/users/urls.py
@@ -10,7 +10,7 @@ router.register(r'announcements', AnnouncementsList)
 urlpatterns=[
      path('api/', include(router.urls)),
     path('signup/tm/', TmSignupView.as_view()),
-    path('signup/student/', StudentSignupView.as_view()),
+    path('addstudents/', StudentSignupView.as_view()),
     path('login/', CustomAuthToken.as_view(), name='auth-token'),
     path('logout/', LogoutView.as_view(), name='logout-view'),
     path('tm/dashboard/', TmOnlyView.as_view(), name='tmdashboard'),
